7f27ba34acf7a701519f3b483362e1e1fac95b91
sssr33 <sskr33@gmail.com>
2014-12-16 00:16:47 +0200

    Added empty universal app

---
+++ NativeLibraries/Universal/RendererWinRT/RendererWinRT.Shared/Class1.cpp
@@ -0,0 +1,11 @@
+﻿#include "pch.h"
+#include "Class1.h"
+
+
+
+using namespace RendererWinRT;
+using namespace Platform;
+
+Class1::Class1()
+{
+}
---
+++ NativeLibraries/Universal/RendererWinRT/RendererWinRT.Shared/Class1.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+namespace RendererWinRT
+{
+	public ref class Class1 sealed
+	{
+	public:
+		Class1();
+	};
+}
