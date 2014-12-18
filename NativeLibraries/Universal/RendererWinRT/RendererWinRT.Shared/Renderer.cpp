bd4d08e615de3fc75f2f341cf1cef2afec536da8
sssr33 <sskr33@gmail.com>
2014-12-19 00:09:37 +0200

    Added stubs for basic event handlers.

--- NativeLibraries/Universal/RendererWinRT/RendererWinRT.Shared/Renderer.cpp
+++ NativeLibraries/Universal/RendererWinRT/RendererWinRT.Shared/Renderer.cpp
@@ -1,7 +1,15 @@
 #include "pch.h"
 #include "Renderer.h"
 
+using namespace Platform;
+using namespace Windows::ApplicationModel;
+using namespace Windows::Foundation;
+using namespace Windows::UI::Core;
+using namespace Windows::UI::Input;
+using namespace Windows::UI::Xaml;
 using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::Graphics::Display;
+using namespace Windows::System::Threading;
 
 namespace RendererWinRT{
 
@@ -9,11 +17,120 @@ namespace RendererWinRT{
 	}
 
 	Renderer::~Renderer(){
+		this->coreInput->Dispatcher->StopProcessEvents();
 	}
 
 	void Renderer::Initialize(SwapChainPanel ^panel){
+		this->InitVisualEventHandlers(panel);
+		this->InitInputEventHandlers(panel);
+		this->InitAppEventHandlers();
+	}
+
+	void Renderer::InitVisualEventHandlers(SwapChainPanel ^panel){
 		auto window = Windows::UI::Xaml::Window::Current->CoreWindow;
+		auto currentDisplayInformation = DisplayInformation::GetForCurrentView();
+
+		window->VisibilityChanged +=
+			ref new TypedEventHandler<CoreWindow^, VisibilityChangedEventArgs^>(this, &Renderer::OnVisibilityChanged);
+
+		currentDisplayInformation->DpiChanged +=
+			ref new TypedEventHandler<DisplayInformation^, Object^>(this, &Renderer::OnDpiChanged);
+
+		currentDisplayInformation->OrientationChanged +=
+			ref new TypedEventHandler<DisplayInformation^, Object^>(this, &Renderer::OnOrientationChanged);
+
+		DisplayInformation::DisplayContentsInvalidated +=
+			ref new TypedEventHandler<DisplayInformation^, Object^>(this, &Renderer::OnDisplayContentsInvalidated);
+
+		panel->CompositionScaleChanged +=
+			ref new TypedEventHandler<SwapChainPanel^, Object^>(this, &Renderer::OnCompositionScaleChanged);
+
+		panel->SizeChanged +=
+			ref new SizeChangedEventHandler(this, &Renderer::OnSwapChainPanelSizeChanged);
+	}
+
+	void Renderer::InitInputEventHandlers(SwapChainPanel ^panel){
+		// Disable all pointer visual feedback for better performance when touching.
+		auto pointerVisualizationSettings = PointerVisualizationSettings::GetForCurrentView();
+		pointerVisualizationSettings->IsContactFeedbackEnabled = false;
+		pointerVisualizationSettings->IsBarrelButtonFeedbackEnabled = false;
+
+		// Register our SwapChainPanel to get independent input pointer events
+		auto workItemHandler = ref new WorkItemHandler([=](IAsyncAction ^)
+		{
+			// The CoreIndependentInputSource will raise pointer events for the specified device types on whichever thread it's created on.
+			this->coreInput = panel->CreateCoreIndependentInputSource(
+				Windows::UI::Core::CoreInputDeviceTypes::Mouse |
+				Windows::UI::Core::CoreInputDeviceTypes::Touch |
+				Windows::UI::Core::CoreInputDeviceTypes::Pen
+				);
+
+			// Register for pointer events, which will be raised on the background thread.
+			this->coreInput->PointerPressed += ref new TypedEventHandler<Object^, PointerEventArgs^>(this, &Renderer::OnPointerPressed);
+			this->coreInput->PointerMoved += ref new TypedEventHandler<Object^, PointerEventArgs^>(this, &Renderer::OnPointerMoved);
+			this->coreInput->PointerReleased += ref new TypedEventHandler<Object^, PointerEventArgs^>(this, &Renderer::OnPointerReleased);
+
+			// Begin processing input messages as they're delivered.
+			this->coreInput->Dispatcher->ProcessEvents(CoreProcessEventsOption::ProcessUntilQuit);
+		});
+
+		// Run task on a dedicated high priority background thread.
+		this->inputLoopWorker = ThreadPool::RunAsync(workItemHandler, WorkItemPriority::High, WorkItemOptions::TimeSliced);
+	}
+
+	void Renderer::InitAppEventHandlers(){
 		auto app = Windows::UI::Xaml::Application::Current;
+
+		app->Suspending += ref new SuspendingEventHandler(this, &Renderer::OnSuspending);
+		app->Resuming += ref new EventHandler<Object^>(this, &Renderer::OnResuming);
+	}
+
+	// Window event handlers.
+	void Renderer::OnVisibilityChanged(CoreWindow^ sender, VisibilityChangedEventArgs^ args){
+
+	}
+
+	// Display event handlers.
+	void Renderer::OnDpiChanged(DisplayInformation^ sender, Object^ args){
+
+	}
+
+	void Renderer::OnOrientationChanged(DisplayInformation^ sender, Object^ args){
+
+	}
+
+	void Renderer::OnCompositionScaleChanged(SwapChainPanel^ sender, Object^ args){
+
+	}
+
+	void Renderer::OnSwapChainPanelSizeChanged(Object^ sender, SizeChangedEventArgs^ e){
+
+	}
+
+	void Renderer::OnDisplayContentsInvalidated(DisplayInformation^ sender, Object^ args){
+
+	}
+
+	// Independent input handling functions.
+	void Renderer::OnPointerPressed(Object^ sender, PointerEventArgs^ e){
+
+	}
+
+	void Renderer::OnPointerMoved(Object^ sender, PointerEventArgs^ e){
+
+	}
+
+	void Renderer::OnPointerReleased(Object^ sender, PointerEventArgs^ e){
+
+	}
+
+	// App events
+	void Renderer::OnSuspending(Object^ sender, SuspendingEventArgs^ e){
+
+	}
+
+	void Renderer::OnResuming(Object ^sender, Object ^args){
+
 	}
 
 }
--- NativeLibraries/Universal/RendererWinRT/RendererWinRT.Shared/Renderer.h
+++ NativeLibraries/Universal/RendererWinRT/RendererWinRT.Shared/Renderer.h
@@ -9,6 +9,33 @@ namespace RendererWinRT{
 		virtual ~Renderer();
 
 		void Initialize(Windows::UI::Xaml::Controls::SwapChainPanel ^panel);
+	private:
+		// Track our independent input on a background worker thread.
+		Windows::Foundation::IAsyncAction^ inputLoopWorker;
+		Windows::UI::Core::CoreIndependentInputSource^ coreInput;
+
+		void InitVisualEventHandlers(Windows::UI::Xaml::Controls::SwapChainPanel ^panel);
+		void InitInputEventHandlers(Windows::UI::Xaml::Controls::SwapChainPanel ^panel);
+		void InitAppEventHandlers();
+
+		// Window event handlers.
+		void OnVisibilityChanged(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::VisibilityChangedEventArgs^ args);
+
+		// Display event handlers.
+		void OnDpiChanged(Windows::Graphics::Display::DisplayInformation^ sender, Platform::Object^ args);
+		void OnOrientationChanged(Windows::Graphics::Display::DisplayInformation^ sender, Platform::Object^ args);
+		void OnCompositionScaleChanged(Windows::UI::Xaml::Controls::SwapChainPanel^ sender, Platform::Object^ args);
+		void OnSwapChainPanelSizeChanged(Platform::Object^ sender, Windows::UI::Xaml::SizeChangedEventArgs^ e);
+		void OnDisplayContentsInvalidated(Windows::Graphics::Display::DisplayInformation^ sender, Platform::Object^ args);
+
+		// Independent input handling functions.
+		void OnPointerPressed(Platform::Object^ sender, Windows::UI::Core::PointerEventArgs^ e);
+		void OnPointerMoved(Platform::Object^ sender, Windows::UI::Core::PointerEventArgs^ e);
+		void OnPointerReleased(Platform::Object^ sender, Windows::UI::Core::PointerEventArgs^ e);
+
+		// App events
+		void OnSuspending(Platform::Object^ sender, Windows::ApplicationModel::SuspendingEventArgs^ e);
+		void OnResuming(Platform::Object ^sender, Platform::Object ^args);
 	};
 
 }
